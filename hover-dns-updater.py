f083fbc82845eaf93690204244f052eaa092ff1b
Chris Kast <ckast@Chriss-MacBook-Pro-2.local>
2019-03-10 13:34:26 -0700

    Change from ipgetter to simply send a request to ipify

--- hover-dns-updater.py
+++ hover-dns-updater.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """hover.py: Provides dynamic DNS functionality for Hover.com using their unofficial API.
    This script is based off one by Andrew Barilla: https://gist.github.com/andybarilla/b0dd93e71ff18303c059"""
@@ -12,7 +12,7 @@ __email__ = ""
 __status__ = "Alpha"
 
 import datetime
-import ipgetter
+# import ipgetter
 import json
 import logging
 import logging.handlers
@@ -141,7 +141,7 @@ class HoverAPI(object):
                     self._current_dns_ips[entry['id']] = entry['content']
 
     def update(self):
-        current_external_ip = ipgetter.myip()
+        current_external_ip = requests.get('https://api.ipify.org').text
         logging.info('Updating - Current external IP = {0}'.format(current_external_ip))
         for dns_id in self._current_dns_ips:
             logging.debug('    {0} = {1}'.format(dns_id, self._current_dns_ips[dns_id]))
